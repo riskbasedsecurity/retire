e0c3b03d27c1c23193f50c28c1c4bf30127fefb9
Jason Lyles <jason.lyles@rtslabs.com>
2023-03-16 16:27:06 -0400

    Load aggregation file

--- lib/tire/version.rb
+++ lib/tire/version.rb
@@ -1,9 +1,10 @@
 module Tire
-  VERSION   = "0.6.2"
+  VERSION   = "0.6.3"
 
   CHANGELOG =<<-END
     IMPORTANT CHANGES LATELY:
 
+    Added support for Aggregations
     19e524c [ACTIVEMODEL] Exposed the response from `MyModel#.update_index` as the `response` method on return value
     bfcde21 [#916] Added support for the Suggest API (@marc-villanueva)
     710451d [#857] Added support for the Suggest API (@fbatista)
